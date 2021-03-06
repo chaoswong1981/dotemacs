9aa0a23ab7c09027682b31b01cc3baf12dde1d8f
tony <chaoswong@qq.com>
2021-03-06 14:54:18 +0800

    remove unuse config

--- init.el
+++ init.el
@@ -1,14 +1,22 @@
+;;基本配置
+(tool-bar-mode -1)
+
+(show-paren-mode t)
+(column-number-mode 1)
+
+(setq make-backup-files nil)
+
+(when (eq system-type 'windows-nt)
+  (set-frame-font "consolas-11"))
+
+;;添加第三方加载路径
 (defvar home-dir (file-name-directory load-file-name)
   "emacs home directory")
-(defvar modules-dir (concat home-dir "modules/")
-  "all of my own el files")
+
 (defvar vendor-dir (concat home-dir "vendor/")
   "all of 3rd party")
-
-(add-to-list 'load-path modules-dir)
 (add-to-list 'load-path vendor-dir)
 
-(require 'my-customs)
-(require 'my-modes)
-
-(setq frame-title-format "%f")
+;;键绑定
+(global-set-key (kbd "C-<tab>") 'dabbrev-expand)
+(define-key minibuffer-local-map (kbd "C-<tab>") 'dabbrev-expand)
